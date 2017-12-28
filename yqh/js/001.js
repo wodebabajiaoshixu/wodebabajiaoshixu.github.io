a046028befb047c2fa6513003ade75572372f38a
wodebabajiaoshixu <1436909213@qq.com>
2017-12-28 18:04:34 +0800

    Revert "123"
    This reverts commit d95ad58bbdeaf65c52fabbb3913c123107e77cde.

---
+++ yqh/js/001.js
@@ -0,0 +1,33 @@
+ var swiper = new Swiper('.swiper-container', {
+      direction: 'vertical',
+      scrollbar: {
+        el:'.swiper-scrollbar',
+        hide: true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+     on:{
+      init: function(){
+        swiperAnimateCache(this); //隐藏动画元素 
+        swiperAnimate(this); //初始化完成开始动画
+      }, 
+      slideChangeTransitionEnd: function(){ 
+        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+      } 
+    }
+    });
+     var img=document.querySelector("#img");
+     var mp3Btn=document.getElementById("mp3Btn")
+     var ff=1;
+        img.onclick=function(){
+            if(ff==1){
+                img.style.animation="none"
+                mp3Btn.pause();
+                ff=0;
+            }else{
+                img.style.animation="img 5s linear infinite"
+                mp3Btn.play();
+                ff=1;
+            }
+        }
